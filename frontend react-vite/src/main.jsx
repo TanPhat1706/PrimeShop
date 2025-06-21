4d928df2112a1b196b1b4d545a9feb8eafdef5ee
TanPhat1706 <tanphaaat@gmail.com>
2025-06-21 10:09:26 +0700

    Commit for deploy

--- frontend react-vite/src/main.jsx
+++ frontend react-vite/src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './global.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
