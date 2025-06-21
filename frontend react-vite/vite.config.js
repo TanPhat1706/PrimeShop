73aefc200d26f724be19db9fc4db0f987fb7572d
TanPhat1706 <tanphaaat@gmail.com>
2025-06-21 12:21:27 +0700

    Vercel fix

--- frontend react-vite/vite.config.js
+++ frontend react-vite/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'VITE_DEPLOY_DEMO' || '/',
   plugins: [react()],
   resolve: {
     alias: {
